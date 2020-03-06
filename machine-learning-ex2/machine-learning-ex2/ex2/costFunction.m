4ae572686ea7a13ecb3eab88db30335da38b9981
Pham Duc Hoang <phamduchoangeee@gmail.com>
2020-03-06 17:44:37 +0900

    ex2: cost function update

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
@@ -20,14 +20,25 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Calcuate J (cost function)
 total_cost = 0;
 for i = 1:m
 	part_1 = log(sigmoid(dot(transpose(theta) , X(i,:))));
 	part_2 = log(1 - sigmoid(dot(transpose(theta) , X(i,:))));
 	total_cost = total_cost + (-y(i,1) * part_1 - (1 - y(i,1)) * part_2 );
 endfor
-
 J = (1/m) * total_cost;
+
+% Calculate grad (Gradient)
+
+for j = 1:(size(theta))
+	total_gradient = 0;
+	for i = 1:m
+		h=sigmoid(dot(transpose(theta) , X(i,:)));
+		total_gradient = total_gradient + (h - y(i,1)) * X(i,j);
+	endfor
+	grad(j,1) = (1/m) * total_gradient;
+endfor
 
 % =============================================================
 
