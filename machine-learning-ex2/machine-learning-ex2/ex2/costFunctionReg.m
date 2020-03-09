3a8fc234c2c5b7d6c7ddf7965b302d471e2fe503
Pham Duc Hoang <36380799+phamduchoangeee@users.noreply.github.com>
2020-03-09 22:36:14 +0900

    Update ex2

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
@@ -24,18 +24,28 @@ for i = 1:m
 	part_2 = log(1 - sigmoid(dot(transpose(theta) , X(i,:))));
 	total_cost = total_cost + (-y(i,1) * part_1 - (1 - y(i,1)) * part_2 ) ;
 end
-
-theta
-n = length(theta)
+% Regularization part
+n = length(theta);
 total_theta =0;
 for i=2:n
-	total_theta = total_theta + theta(i,1) * theta(i,1)
+	total_theta = total_theta + theta(i,1) * theta(i,1);
 end
-
+% Conclusion for J
 J = (1/m) * total_cost + total_theta*lambda/(2*m);
 
-
-
+% Gradient Part
+for j = 1:(length(theta))
+	total_gradient = 0;
+	for i = 1:m
+		h=sigmoid(dot(transpose(theta) , X(i,:)));
+		total_gradient = total_gradient + (h - y(i,1)) * X(i,j);
+	endfor
+	if j==1
+		grad(j,1) = (1/m) * total_gradient;
+	else	
+		grad(j,1) = (1/m) * total_gradient + (lambda/m)* theta(j,1);
+	end
+end
 
 % =============================================================
 
